90deb2d4f52952518e37943cdb4367344799e1d1
Karan Singh Chandel <123291197+karan0207@users.noreply.github.com>
2025-03-18 22:21:41 +0530

    add image and updated bg

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -37,7 +37,7 @@ const HeroSection = () => {
 
                     <HeroRightContainer id="Right">
 
-                        <Img src="/karan-bg(1)(1).png" alt="hero-image" className='dark' />
+                        <Img src="/karan-main-pic.png" alt="hero-image" className='dark' />
                     </HeroRightContainer>
                 </HeroInnerContainer>
 
